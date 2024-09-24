db16115013cde1918bd379a4b24a29af7cb65490
joaovitormachadodev99@gmail.com <joaovitormachadodev99@gmail.com>
2024-09-24 17:48:13 -0300

    ex de soma e multiplicação

---
+++ exsoma.py
@@ -0,0 +1,7 @@
+n1 = float(input("Digite o primeiro valor: "))
+n2 = float(input("Digite o segundo valor: "))
+n3 = float(input("Digite o terceiro valor: "))
+
+soma = (n1 + n2) * n3 
+
+print(f"O resutado de {n1} + {n2} X {n3} é de {soma}")
