f006626528b4151edd47df039ff6e4fbb9549e79
joaovitormachadodev99@gmail.com <joaovitormachadodev99@gmail.com>
2024-09-24 17:20:48 -0300

    att

--- ex12.py
+++ ex12.py
@@ -1,5 +1,5 @@
 reais = float(input("Quantos reais você tem? "))
 valor_dolar = float(input("Quanto está o valor do Dólar atualmente? "))
-dolar = reais / valor_dolar
+dolar = float(reais / valor_dolar)
 print(f"A converção de {reais} reais é de {dolar:.2f} dólares.")
 
---
+++ exnotas.py
@@ -0,0 +1,8 @@
+nome = str(input("Digite o nome: "))
+idade = int(input("Digite a idade: "))
+s = str(input("Digite o sexo: "))
+n1 = float(input("Digite a primeira nota: "))
+n2 = float(input("Digite a segunda nota: "))
+media = (n1 + n2) / 2
+
+print(f"Nome do aluno(a): {nome}, idade: {idade}, sexo: {s}, primeira nota: {n1}, segunda nota: {n2}, média de {media}")
