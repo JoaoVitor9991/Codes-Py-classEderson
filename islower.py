a78b79a968ae208e97653fe1b15a1c232211b1d9
joaovitormachadodev99@gmail.com <joaovitormachadodev99@gmail.com>
2024-09-26 15:29:42 -0300

    islowe e isupper

---
+++ islower.py
@@ -0,0 +1,9 @@
+#islower ELA VERFICA TODA A VARIAVEL SE É MINÚSCULA E IMPRIME TRUE OU FALSE
+
+
+a = "JOAO VITOR"
+print("lower",a.islower())
+
+#isupper VERIFICA SE TODA A VARIAVEL É MAIÚSCULA E IMPRIME TRUE E FALSE
+b = "joao vitor"
+print("upper", b.islower())
