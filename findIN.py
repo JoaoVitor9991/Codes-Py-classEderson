7cc240040ad894a7b73437b9a06adcca52cb1cf8
joaovitormachadodev99@gmail.com <joaovitormachadodev99@gmail.com>
2024-09-26 16:15:24 -0300

    find in e colchete

---
+++ findIN.py
@@ -0,0 +1,13 @@
+#find RETORNA ONDE A SUBSTRING COMEÇA NA STRING ("PRIMEIRA")
+
+a = "João Vitor Machado Oliveira"
+print(a.find("Vitor"))
+
+#in USADO PARA VERIFICAR SE UMA SUBSTRING É PARTE DE UMA OUTRA STRING
+a ="João Vitor Machado"
+print("Machado" in a, "in")
+
+#count RETORNA A FREQUÊNCIA DE OCORRÊNCIA DO PARÂMETRO PASSADO 
+a = "João Vitor Machado Oliveira".upper()
+print(a.count("O")) 
+
