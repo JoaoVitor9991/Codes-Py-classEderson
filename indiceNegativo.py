e452ad2027533166e5474efbf5cf47a39e90c77a
joaovitormachadodev99@gmail.com <joaovitormachadodev99@gmail.com>
2024-09-26 16:52:34 -0300

    indinegativo

---
+++ indiceNegativo.py
@@ -0,0 +1,16 @@
+s = "Olá, Mundo!"
+print(s[-11])
+print(s[-9])
+print(s[-5])
+print("--------------------------------------")
+#slice é a fatia ex abaixo
+
+s = "Olá, Mundo!"
+print(s[1:3])
+
+print("----------------------------------------")
+
+#se omitirmos o indice de inicio da fatia ou o de final (ou ambos), o ínicio e o final da string será considerados
+
+b = "Olá, Mundo!"
+print(b[:3])
