bbc8b16da945380b450d32e7ce2489ed3d33dcf5
joaovitormachadodev99@gmail.com <joaovitormachadodev99@gmail.com>
2024-09-26 15:41:44 -0300

    att

---
+++ isdigit.py
@@ -0,0 +1,7 @@
+#isdigit VERIFICA SE A NÚMEROS INTEIROS NA VARIAVEL E IMPRIMIR VERDADEIRO OU FALSO
+
+a = "12345"
+print(a.isdigit())
+
+b = "12345abcde"
+print(b.isdigit())
---
+++ replace.py
@@ -0,0 +1,10 @@
+#replace é usado para substituir uma item na variavel
+a = "Ederson Roberto"
+print(a.replace("Roberto", "Costa"))
+
+
+
+#split é usado para separar uma string usando sep como separador 
+
+a = "Ederson-Roberto-Costa"
+print(a.split("-"))
