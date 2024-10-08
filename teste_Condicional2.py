2a1ef40d595fe9d23188b70b39191a2871cb52f8
joaovitormachadodev99@gmail.com <joaovitormachadodev99@gmail.com>
2024-10-08 17:31:07 -0300

    att

--- teste_Condicional2.py
+++ teste_Condicional2.py
@@ -3,5 +3,7 @@ n2 = int(input("Digite outro número: "))
 
 if n1 > n2:
     print(f"O número {n1} é maior que {n2}")
-else:
+elif n2 > n1:
     print(f"O número {n2} é maior que {n1}")
+else:
+    print(f"Os números não podem ser iguais. {n1} e {n2}")
---
+++ votar.py
@@ -0,0 +1,5 @@
+ano = int(input("Digite o ano que você nasceu: "))
+if ano <= 2008:
+    print("Você pode votar.")
+else:
+    print("Você não pode votar")
