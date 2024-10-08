8dd15f07e90a3225f5be7fc5e7d1a194bf5ce5e3
joaovitormachadodev99@gmail.com <joaovitormachadodev99@gmail.com>
2024-10-08 14:44:47 -0300

    att

---
+++ inAvancado.py
@@ -0,0 +1,8 @@
+frutas = ["maca", "laranja", "banana", "cereja"]
+
+print([1,2] + [3, 4])
+print(frutas +[6, 7, 8, 9])
+
+print([] * 4)
+print([1, 2, ["Olá", ["Ola, ", "adeus "]]]*2) 
+
---
+++ lista.py
@@ -0,0 +1,24 @@
+# Os itens em uma lista pode ser diferentes 
+
+lista = ["João", 25, 1.77, 106.7]
+print(lista)
+
+lista1 = [10, 20, 30, 40]
+lista2 = ["spam", "bungee", "swallow"]
+print (lista1, lista2)
+
+#lista aninhada 
+lista3 = ["Oi", 2,0, 5, [10,20]]
+print(lista3)
+
+#Lista com len, o len não conta quantos elementos está dentro da sub lista
+lista4 = ["Oi", 2.0, 5, [10, 20]]
+print(len(lista4))
+
+
+numeros = [17, 123, 87, 34, 66, 8398, 44]
+print(numeros[2])
+print(numeros[9-8])
+print(numeros[-2])
+print(numeros[-1])
+
