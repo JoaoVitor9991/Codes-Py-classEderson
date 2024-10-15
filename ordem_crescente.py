1f361be189a0f7ed180954151d6222fd49be490b
joaovitormachadodev99@gmail.com <joaovitormachadodev99@gmail.com>
2024-10-15 14:34:56 -0300

    att

--- ordem_crescente.py
+++ ordem_crescente.py
@@ -2,10 +2,19 @@ n1 = int(input("Digite o primeiro valor: "))
 n2 = int(input("Digite o segundo valor: "))
 n3 = int(input("Digite o terceiro valor: "))
 
-if n1 < n2 and n1 < n2 and n2 < n3:
+if n1 < n2 and n2 < n3:
     print(f"{n1}, {n2}, {n3}")
 elif n1 < n3 and n3 < n2:
     print(f"{n1}, {n2}, {n3}")
+elif n2 < n1 and n1 < n3:
+    print(f"{n1}, {n2}, {n3}")
+elif n2 < n3 and n3 < n1:
+    print(f"{n1}, {n2}, {n3}")
+elif n2 < n1 and n1 < n3:
+    print(f"{n1}, {n2}, {n3}")
+elif n2 < n3 and n3 < n1:
+    print(f"{n1}, {n2}, {n3}")
+ 
     
 
     
