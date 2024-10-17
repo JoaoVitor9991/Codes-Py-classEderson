2bb1ee1e3bc134530ced7e7f7403c54e52e1635c
joaovitormachadodev99@gmail.com <joaovitormachadodev99@gmail.com>
2024-10-17 15:43:46 -0300

    ex de while

--- gasolina.py
+++ gasolina.py
@@ -1,11 +1,22 @@
-gasolina_ou_alcool = str(input("G para gasolina ou A para Alcool "))
+gasolina_ou_alcool = str(input("G para gasolina ou A para Alcool ").upper())
 litro = float(input("Qual a quantidade em litros? "))
 
-if gasolina_ou_alcool.upper() == "G":
+if gasolina_ou_alcool == "A":
+    preco_por_litro = 1.90
     if litro <= 20:
-        valor = litro * 0.04
-        print(valor)
-    elif litro > 20:
-        valor = litro * 0.06
-        print(valor)
+        desconto = 0.03
+    else:
+        desconto = 0.05
+elif gasolina_ou_alcool == "G":
+    preco_por_litro = 2.50
+    if litro <= 20:
+        desconto = 0.04
+    else:
+        desconto = 0.06
+else:
+    print("Tipo invalido! ")
+    print("Desconto = 0")
+    print("Valor = 0")
+
+    valor_sem_desconto = litro * preco_por_litro
     
---
+++ while2.py
@@ -0,0 +1,9 @@
+while True:
+    valor = int(input("Digite o valor 1 ou 0 para encerrar"))
+    if valor >=1:
+        continue
+        print("Maior que um ")
+    if valor >=1:
+        print("maior que um ")
+
+        #quando encontro um continue ele sempre ira voltar para o inicio, no exemplo acima tudo que está abaixo do CONTINUE não irá executar.
