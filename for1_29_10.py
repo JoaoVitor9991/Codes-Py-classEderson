da4ae6c4010bcf7bd2430357681e7fecbc84de5b
joaovitormachadodev99@gmail.com <joaovitormachadodev99@gmail.com>
2024-10-29 17:05:36 -0300

    att

---
+++ for1_29_10.py
@@ -0,0 +1,7 @@
+
+for i in range (0, 20):
+    i = i +1
+    print(i)
+    #print(i,  end=" ")
+
+
---
+++ time_lista.py
@@ -0,0 +1,5 @@
+time = ["Palmeiras", "Curitiba", "São Paulo"]
+i=0
+for n in time:
+    i = i + 1
+    print(i,"-",n)
